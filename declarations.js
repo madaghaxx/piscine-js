3136c240cb98b492ef44f9dc6097ef1c9670005c
aberhili <aberhili0man@gmail.com>
2024-12-18 09:52:35 +0100

    third

--- declarations.js
+++ declarations.js
@@ -1,4 +1,8 @@
+const  str="str"
+const num= 0
+const bool = false
 const escapeStr = "\`\\\/\"\'"
 const arr = [4, '2']
-const obj = { str: "str", num: 0, bool: false, undef: undefined }
-const nested = { arr: [4,undefined,'2'], obj: { str: "str", num: 0, bool: false } }
+const obj = { str, num, bool, undef: undefined }
+
+const nested = {  arr : [4, undefined, '2'], obj: {  str, num, bool } }
