2352780ce3c55b4948977448b982b7c097b97d07
aberhili <aberhili0man@gmail.com>
2024-12-18 10:21:30 +0100

    4

--- mutability.js
+++ mutability.js
@@ -1,6 +1,12 @@
-const clone1 = person
-Object.freeze(clone1.age,clone1.name,clone1.country)
-const clone2 = person
-Object.freeze(clone2.age,clone2.name,clone2.country)
-const samePerson = person
-person.age = 78
+const clone1 = {
+    name: 'Rick',
+    age: 77,
+    country: 'US',
+}
+const clone2 = {
+    name: 'Rick',
+    age: 77,
+    country: 'US',
+}
+  
+const samePerson=person
