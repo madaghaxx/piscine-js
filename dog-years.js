962ec169776a5147d9bfbaea205b663080d3c3b7
aberhili <aberhili0man@gmail.com>
2024-12-18 11:32:53 +0100

    Update

---
+++ dog-years.js
@@ -0,0 +1,9 @@
+const earth = 1
+const mercury = 0.2408467
+const venus = 0.61519726
+const mars = 1.8808158
+const jupiter = 11.862615
+const saturn = 29.447498
+const uranus = 84.016846
+const neptune = 164.79132
+const dogYears = (naame, age) => (naame * age * 7) / 60 / 60 / 24 / 365.25
