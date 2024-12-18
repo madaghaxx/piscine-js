d939be15861a5867f1c01709bc5f282a151ccd22
aberhili <aberhili0man@gmail.com>
2024-12-18 10:32:19 +0100

    4

--- mutability.js
+++ mutability.js
@@ -1,6 +1,7 @@
 const clone1 = person, clone2 = person, samePerson = person
-person.age = 78
-person.country = 'FR'
 Object.freeze(clone1)
 Object.freeze(clone2)
+person.age = 78
+person.country = 'FR'
+
 
