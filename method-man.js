fb211709f280f5d796268c1463049be570d2c6d9
aberhili <aberhili0man@gmail.com>
2024-12-18 13:43:15 +0100

    Update

--- method-man.js
+++ method-man.js
@@ -8,7 +8,7 @@ const yell = (str) => {
     return str.toUpperCase()
 }
 const whisper = (str) => {
-    return str.toLowerCase()
+    return ("*" + str.toLowerCase() + "*")
 }
 const capitalize = (str) => {
     return str.charAt(0).toUpperCase() + str.slice(1)
