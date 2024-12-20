6c32c154b20eda8b3104945136c56891f10a6bc1
aberhili <aberhili0man@gmail.com>
2024-12-20 11:22:01 +0100

    update

--- elementary.js
+++ elementary.js
@@ -1,3 +1,16 @@
-let multiply = (a, b) => { let res = 0; for (let i = 0; i < b; i++) { res += a; } return res; }
+let multiply = (a, b) => {
+    let res = 0;
+    if (a < 0 && b < 0) { a = -a; b = -b; }
+    else if (a < 0) { a = -a; b = -b; }
+    else if (b < 0) { a = -a; b = -b; }
+    for (let i = 0; i < b; i++) { res += a; } return res;
+}
+// console.log(multiply(123, -22))
+
+
 let divide = (a, b) => { let res = 0; while (a >= b) { a -= b; res++; } return res; }
+// console.log(divide(10, 3)) // 3
+
+
 let modulo = (a, b) => { while (a >= b) { a -= b; } return a; }
+// console.log(modulo(10, 3)) // 1
