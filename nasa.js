06fa81a050c4fdf89d5ad3ba38a29d4e76f53a5b
aberhili <aberhili0man@gmail.com>
2024-12-22 11:41:13 +0100

    update

--- nasa.js
+++ nasa.js
@@ -2,7 +2,7 @@ let nasa = (N) => {
     let res = ""
     for (let i = 1; i <= N; i++) {
         if (i % 3 === 0 && i % 5 === 0) {
-            res += "NASA ";
+            res += "NASA";
         } else if (i % 3 === 0) {
             res += "NA ";
         } else if (i % 5 === 0) {
