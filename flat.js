3456c7e9907258cd6450bc169d5e9e10351824c3
aberhili <aberhili0man@gmail.com>
2024-12-22 16:38:33 +0100

    update

--- flat.js
+++ flat.js
@@ -1,6 +1,7 @@
 let flat = (a, depth) => {
     let res = [];
-    depth ? depth : 1;
+    depth ? depth : depth = 1;
+    // console.log(depth)
     for (let i = 0; i < a.length; i++) {
         if (Array.isArray(a[i]) && depth > 0) {
             res = res.concat(flat(a[i], depth - 1));
@@ -11,7 +12,6 @@ let flat = (a, depth) => {
     return res;
 }
 
-const arr1 = [0, 1, 2, [3, 4, [5, 6]]];
+// const arr1 = [0, 1, 2, [3, 4, [5, 6]]];
 
-// console.log(flat(arr1, 2)); // [0, 1, 2, 3, 4, [5, 6]]
-// console.log(flat(arr1, 2)); // [0, 1, 2, 3, 4, 5, 6]
+// console.log(flat([1, [2]]))
