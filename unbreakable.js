209a5ba7eaa1e56667fb5313e7c17a775e7109c3
aberhili <aberhili0man@gmail.com>
2024-12-22 11:32:43 +0100

    update

--- unbreakable.js
+++ unbreakable.js
@@ -15,9 +15,17 @@ let join = (arr, separator) => {
 let split = (str, separator) => {
     let result = [];
     let word = '';
+    if (separator === '') {
+        for (let i = 0; i < str.length; i++) {
+            result.push(str[i]);
+        }
+        return result;
+    }
+
     let sepLength = separator.length;
     for (let i = 0; i < str.length; i++) {
-        if (str.substr(i, sepLength) === separator) {
+        // console.log(str.substr(i, sepLength));
+        if ((str[i] == separator[0]) && (str.substr(i, sepLength) === separator)) {
             result.push(word);
             word = '';
             i += sepLength - 1;
@@ -28,5 +36,4 @@ let split = (str, separator) => {
     result.push(word);
     return result;
 }
-// console.log(split('ggg - ddd - b', ' - '));
-// console.log(null || 'string');
+console.log(split('Riad', ''))
