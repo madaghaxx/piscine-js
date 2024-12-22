47d59296a88707858324879387b16dcff5973393
aberhili <aberhili0man@gmail.com>
2024-12-22 11:50:23 +0100

    update

--- get.js
+++ get.js
@@ -2,7 +2,12 @@ let get = (src, path) => {
     let arr = path.split(".");
     let res = src;
     for (let i = 0; i < arr.length; i++) {
+        if (!res[arr[i]]) {
+            return undefined;
+        }
+
         res = res[arr[i]];
     }
     return res;
 }
+// get({ nested: { key: 'value' } }, 'nx.nx')
