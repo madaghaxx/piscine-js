8b1ea76e13f35bc2ebcab70f5fa281df6098aa81
aberhili <aberhili0man@gmail.com>
2024-12-22 17:29:20 +0100

    update

--- script.sh
+++ script.sh
@@ -2,5 +2,5 @@ git remote add github  https://github.com/madaghaxx/piscine-js.git
 git remote add zone01 https://learn.zone01oujda.ma/git/aberhili/piscine-js.git
 git add .
 git commit -m "update"
-git push github main  # Push to GitHub
+git push github master  # Push to GitHub
 git push zone01 master  # Push to the second remote
