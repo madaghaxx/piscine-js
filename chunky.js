e57928bc5044c54cbacba4924bb8d67dba274993
aberhili <aberhili0man@gmail.com>
2024-12-22 11:37:56 +0100

    update

--- chunky.js
+++ chunky.js
@@ -1,4 +1,4 @@
-let chuncky = (arr, size) => {
+let chunk = (arr, size) => {
     let result = [];
     let chunk = [];
     for (let i = 0; i < arr.length; i++) {
---
+++ nasa.js
@@ -0,0 +1,16 @@
+let nasa = (N) => {
+    let res = ""
+    for (let i = 1; i <= N; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            res += "NASA ";
+        } else if (i % 3 === 0) {
+            res += "NA ";
+        } else if (i % 5 === 0) {
+            res += "SA ";
+        } else {
+            res += i + " ";
+        }
+    }
+    return res;
+}
+// console.log(nasa(100));
