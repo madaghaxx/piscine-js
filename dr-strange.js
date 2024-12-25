9b94549467545cdbbc16deaa4f7fd6dd52a4466a
aberhili <aberhili0man@gmail.com>
2024-12-25 10:52:29 +0100

    clearing

--- dr-strange.js
+++ dr-strange.js
@@ -2,7 +2,7 @@ let addWeek = (date) => {
     const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'secondMonday', 'secondTuesday', 'secondWednesday', 'secondThursday', 'secondFriday', 'secondSaturday', 'secondSunday'];
     const epoch = new Date('0001-01-01');
     const time = date - epoch;
-    const day = time / (1000 * 60 * 60 * 24) % 14;
+    const day = time / (1000 * 60 * 60 * 24);
     return days[day];
 }
 let timeTravel = (obj) => {
