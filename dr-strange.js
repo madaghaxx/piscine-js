1028a979b46c184173715d3fa428107ed2dafc46
aberhili <aberhili0man@gmail.com>
2024-12-25 14:11:59 +0100

    update

--- dr-strange.js
+++ dr-strange.js
@@ -1,12 +1,11 @@
+const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'secondMonday', 'secondTuesday', 'secondWednesday', 'secondThursday', 'secondFriday', 'secondSaturday', 'secondSunday'];
 let addWeek = (date) => {
-    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'secondMonday', 'secondTuesday', 'secondWednesday', 'secondThursday', 'secondFriday', 'secondSaturday', 'secondSunday'];
     const epoch = new Date('0001-01-01');
     const time = date - epoch;
     const day = time / (1000 * 60 * 60 * 24)%14;
     return days[day];
 }
 let timeTravel = (obj) => {
-
     obj.date.setHours(obj.hour)
     obj.date.setMinutes(obj.minute)
     obj.date.setSeconds(obj.second)
