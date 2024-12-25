a6e4497ded2c5d77ce54912f9ddfe232bd41bdbf
aberhili <aberhili0man@gmail.com>
2024-12-25 10:52:06 +0100

    clearing

--- dr-strange.js
+++ dr-strange.js
@@ -5,12 +5,8 @@ let addWeek = (date) => {
     const day = time / (1000 * 60 * 60 * 24) % 14;
     return days[day];
 }
-/ console.log(addWeek(new Date('0001-01-09'))) // Output: secondTuesday
-
 let timeTravel = (obj) => {
-    // let hourstosub = obj.date.getHours()
-    // let minutestosub = obj.date.getMinutes()
-    // let secondstosub = obj.date.getSeconds()
+
     obj.date.setHours(obj.hour)
     obj.date.setMinutes(obj.minute)
     obj.date.setSeconds(obj.second)
