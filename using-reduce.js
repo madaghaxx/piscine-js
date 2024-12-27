f7426fb56690fa3749bbd01ee798f50c594d3a2b
aberhili <aberhili0man@gmail.com>
2024-12-27 11:41:43 +0100

    update

--- using-reduce.js
+++ using-reduce.js
@@ -1,18 +1,22 @@
 let adder = (arr, num) => arr.reduce((acc, curr) => acc + curr, num ? num : 0);
 // console.log(adder([1, 2, 3, 4, 5]));
-let sumOrMul = (arr) => {
+
+function sumOrMul(arr, c=0) {
   return arr.reduce((acc, cur) => {
-    if (acc % 2 === 0) {
-      return acc * cur;
-    } else {
-      return acc + cur;
-    }
-  });
-};
+      if (cur % 2 === 0) {
+        acc *= cur;
+      } else {
+         acc += cur;
+      }
+          return acc;
+    }, c
+  );
+}
 
 let funcExec = (arr, func) => {
   return arr.reduce((acc, curr) => {
     return curr(acc);
   }, func);
 };
-console.log(adder([9, 24, 7, 11, 3], 10));
+
+// console.log(sumOrMul([29, 23, 3, 2, 25]));
