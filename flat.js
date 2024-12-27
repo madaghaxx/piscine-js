7c065a7b400df081df59c0a0c17652241299f338
aberhili <aberhili0man@gmail.com>
2024-12-27 12:08:04 +0100

    update

--- flat.js
+++ flat.js
@@ -2,11 +2,13 @@ const flat = (arr, num=1) => {
     let res = [];
   for (let i = 0; i < arr.length; i++) {
     if (Array.isArray(arr[i]) && num > 0) {
-      res = res.concat(flat(arr[i], num - 1));
+        res = res.concat(flat(arr[i], num - 1));
+        console.log(res);
+        
     } else {
       res.push(arr[i]);
     }
   }
   return res;
 };
-// console.log(flat([1, [2]]));
+console.log(flat([1, [2]]));
