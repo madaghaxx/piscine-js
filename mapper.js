d058c1a6790edfc0f7707bac6d9c0fb8907027ef
aberhili <aberhili0man@gmail.com>
2024-12-27 12:21:55 +0100

    update

--- mapper.js
+++ mapper.js
@@ -1,11 +1,22 @@
 let map = (arr, fx) => {
   let res = [];
   for (let i = 0; i < arr.length; i++) {
-    res.push(fx(arr[i]));
+    res.push(fx(arr[i], i, arr));
+  }
+  return res;
+};
+const flat = (arr, num = 1) => {
+  let res = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i]) && num > 0) {
+      res = res.concat(flat(arr[i], num - 1));
+    } else {
+      res.push(arr[i]);
+    }
   }
   return res;
 };
 
 let flatMap = (arr, fx) => {
-  return flat(mapper(arr, fx));
+  return flat(map(arr, fx));
 };
