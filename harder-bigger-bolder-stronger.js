12848c20eb51abd6e6dce8ce5476e2238a48f0b6
aberhili <aberhili0man@gmail.com>
2024-12-28 19:06:44 +0100

    update

--- harder-bigger-bolder-stronger.js
+++ harder-bigger-bolder-stronger.js
@@ -9,7 +9,7 @@ export let generateLetters = () => {
     if (i < 40) {
       div.style.fontWeight = 300;
     }
-    if (i > 40 && i < 80) {
+    if (i > 40 && i <= 80) {
       div.style.fontWeight = 400;
     }
     if (i > 80) {
