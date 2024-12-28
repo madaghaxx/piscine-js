41de596f8c9609583c4e14d5e0d56c91fd187625
aberhili <aberhili0man@gmail.com>
2024-12-28 12:49:17 +0100

    update

--- get-them-all.js
+++ get-them-all.js
@@ -2,13 +2,14 @@ export let getArchitects = () => {
   let res = [];
   let res2 = [];
   let arr1 = document.getElementsByTagName("a");
-  let arr2 = document.getElementsByTagName(":not(a)");
+  let arr2 = document.getElementsByTagName("span");
   for (let i = 0; i < arr1.length; i++) {
     res.push(arr1[i]);
   }
   for (let i = 0; i < arr2.length; i++) {
     res2.push(arr2[i]);
   }
+
   return [res, res2];
 };
 
@@ -16,7 +17,7 @@ export let getClassical = () => {
   let res = [];
   let res2 = [];
   let arr1 = document.getElementsByClassName("classical");
-  let arr2 = document.getElementsByClassName(":not(classical)");
+  let arr2 = document.querySelectorAll("a:not(.classical)");
   for (let i = 0; i < arr1.length; i++) {
     res.push(arr1[i]);
   }
@@ -29,8 +30,8 @@ export let getClassical = () => {
 export let getActive = () => {
   let res = [];
   let res2 = [];
-  let arr1 = document.getElementsByClassName("classical.active");
-  let arr2 = document.getElementsByClassName(":not(classical.active)");
+  let arr1 = document.getElementsByClassName("active");
+  let arr2 = document.querySelectorAll("a:not(.active)");
   for (let i = 0; i < arr1.length; i++) {
     res.push(arr1[i]);
   }
@@ -44,7 +45,7 @@ export let getBonannoPisano = () => {
   let res = [];
 
   let arr1 = document.getElementById("BonannoPisano");
-  let arr2 = document.getElementsByClassName("classical.active");
+  let arr2 = document.querySelectorAll("a:not(#BonannoPisano)");
 
   for (let i = 0; i < arr2.length; i++) {
     res.push(arr2[i]);
