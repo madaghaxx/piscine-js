1a4d72179d36bf9cc56c29e470fdb8fc5cfed33d
aberhili <aberhili0man@gmail.com>
2024-12-28 18:34:14 +0100

    update

---
+++ build-brick-and-break.js
@@ -0,0 +1,28 @@
+export let build = (num) => {
+  for (let i = 0; i < num; i++) {
+    let div = document.createElement("div");
+    div.id = "brick-" + i;
+    if (i % 3 === 1) {
+      div.setAttribute("foundation", "true");
+    }
+    setTimeout(() => {
+      document.body.append(div);
+    }, i * 100);
+  }
+};
+export let repair = (...num) => {
+  num.forEach((el) => {
+    let hh = document.getElementById(el);
+    if (hh.getAttribute("foundation") === "true") {
+      hh.setAttribute("data-repaired", "in progress");
+    } else {
+      hh.setAttribute("data-repaired", "true");
+    }
+  });
+};
+
+export let destroy = () => {
+  let diz = document.getElementsByTagName("div");
+  let hhhh = diz[diz.length - 1];
+  hhhh.remove();
+};
