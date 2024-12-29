a405eaea3787dfd14ffa080d90f89b4349bff1af
aberhili <aberhili0man@gmail.com>
2024-12-29 01:37:43 +0100

    update

--- pimp-my-style.js
+++ pimp-my-style.js
@@ -1,25 +1,26 @@
 import { styles } from "./pimp-my-style.data.js";
-let num = 0;
-export let pimp = () => {
+let styleIndex = 0;
+export const pimp = () => {
   const btn = document.querySelector("button");
+  if (!btn) return;
 
   // console.log(btn.classList.value.split(" "));
   if (
     !btn.classList.contains("unpimp") &&
     btn.classList.value.split(" ").length < 16
   ) {
-    btn.classList.add(styles[num]);
-    num += 1;
+    btn.classList.add(styles[styleIndex]);
+    styleIndex += 1;
     if (btn.classList.value.split(" ").length === 16) {
       btn.classList.toggle("unpimp");
     }
   } else if (btn.classList.contains("unpimp")) {
-    btn.classList.remove(styles[num]);
-    if (num === 0) {
+    styleIndex -= 1;
+    btn.classList.remove(styles[styleIndex]);
+    if (styleIndex === 0) {
       btn.classList.toggle("unpimp");
     } else {
-      num -= 1;
+      styleIndex -= 1;
     }
   }
 };
-// a === 1 ?? b;
