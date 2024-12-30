6bbcca41e6bfb0020eb0f0cfd33a09cf156d4839
aberhili <aberhili0man@gmail.com>
2024-12-30 13:36:21 +0100

    update

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-!#bin/bash
+#bin/bash
 git remote add github https://github.com/madaghaxx/piscine-js.git
 git remote add zone01 https://learn.zone01oujda.ma/git/aberhili/piscine-js.git
 git config --global credential.helper store
