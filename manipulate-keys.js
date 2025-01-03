1a933e0048409463b5b32d63995dd0527e2a1da8
aberhili <aberhili0man@gmail.com>
2025-01-03 13:13:27 +0100

    update

--- manipulate-keys.js
+++ manipulate-keys.js
@@ -19,15 +19,12 @@ let mapKeys = (obj, func) => {
 
 let reduceKeys = (obj, func, init) => {
   let kk = Object.keys(obj);
-  let res;
-  init ? (res = init) : (res = kk[0]);
-  for (let key in obj) {
-    if (res[0] !== kk[0]) {
-      res = func(res, key);
-    }
+  let res = init !== undefined ? init : kk[0];
+  for (let i = init !== undefined ? 0 : 1; i < kk.length; i++) {
+    res = func(res, kk[i]);
   }
   return res;
 };
 
 const nutrients = { carbohydrates: 12, protein: 20, fat: 5 };
-console.log(reduceKeys(nutrients, (acc, cr) => `${acc}${cr}:`, ":"));
+console.log(reduceKeys(nutrients, (acc, cr) => acc.concat(", ", cr)));
