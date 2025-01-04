3dc9608597af7847f1b8f50be48dc34784cb3038
aberhili <aberhili0man@gmail.com>
2025-01-04 11:45:57 +0100

    update

--- debounce.js
+++ debounce.js
@@ -1,11 +1,25 @@
 let debounce = (func, delay) => {
   let id;
   return function (...args) {
-    if (id) {
-      clearTimeout(id);
-    }
+    clearTimeout(id);
     id = setTimeout(() => {
       func(...args);
+    }, delay);
+  };
+};
+
+let opDebounce = (func, delay, option) => {
+  let timer = null;
+  return function (...args) {
+    // let istrue = false;
+    if (timer === nill && option) {
+      func(...args);
+      //   istrue = true;
+    }
+    clearTimeout(id);
+    id = setTimeout(() => {
+      func(...args);
+      timer = null;
     }, delay);
   };
 };
