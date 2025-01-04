c53a1c8101785d91b1b716860d10846e600b9de7
aberhili <aberhili0man@gmail.com>
2025-01-04 12:15:19 +0100

    update

--- throttle.js
+++ throttle.js
@@ -4,24 +4,22 @@ let throttle = (func, delay) => {
     clearTimeout(id);
     id = setTimeout(() => {
       func(...args);
-      if (option.trailing) {
-        func(...args);
-      }
     }, delay);
   };
 };
 
 let opThrottle = (func, delay, option = {}) => {
-  let timer;
-  let called = false;
+  let id;
+  let lastArgs;
   return function (...args) {
-    if (option.leading && !called) {
-      func(...args);
-      called = true;
-    }
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-      func(...args);
+    lastArgs = args;
+    clearTimeout(id);
+    id = setTimeout(() => {
+      if (option.trailing) {
+        func(...lastArgs);
+      } else {
+        func(...args);
+      }
     }, delay);
   };
 };
