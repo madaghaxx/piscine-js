f479aad68efa9b60b39891ac125748f1944142d7
Aymane Berhili <aberhili0man@gmain.com>
2025-01-06 17:14:53 +0100

    update

--- keep-trying-or-giveup.js
+++ keep-trying-or-giveup.js
@@ -1,7 +1,28 @@
 let retry = (count, callback) => {
-    
-}
+  return function (...args) {
+    return callback(...args)
+      .then((data) => data)
+      .catch((error) => {
+        if (count == 0) {
+          return retry(count - 1, callback)(...args);
+        } else {
+          throw error;
+        }
+      });
+  };
+};
 
 let timeout = (delay, callback) => {
-    
-}
+  return function (...args) {
+    let time = Date.now();
+    return callback(...args)
+      .then((data) => data)
+      .catch(() => {
+        if (Date.now() - time < delay) {
+          return timeout(delay, callback)(...args);
+        } else {
+          throw new Error('Timeout exceeded');
+        }
+      });
+  };
+};
