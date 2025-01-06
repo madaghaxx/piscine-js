ce561fe51c2335b25153fc32efc700812d74b953
Aymane Berhili <aberhili0man@gmain.com>
2025-01-06 15:48:24 +0100

    update

--- interpolation.js
+++ interpolation.js
@@ -2,14 +2,16 @@ let interpolation = (obj) => {
   let stepdelay = obj.duration / obj.step; //2
   let stepgap = (obj.end - obj.start) / obj.step;
   let xANDy = [];
+  let time;
   for (let i = obj.start; i < obj.end; i += stepgap) {
     let x = i;
     let y = stepdelay;
     xANDy.push([x, y]);
   }
   let hh = 0;
-  setTimeout(() => {
+  time = setTimeout(() => {
     obj.callback(xANDy[hh]);
     hh++;
   }, stepdelay);
+  clearTimeout(time);
 };
