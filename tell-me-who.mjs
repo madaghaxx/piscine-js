ff0661c120799ca75800fd9fdc4e94f5f64aa306
Aymane Berhili <aberhili0man@gmain.com>
2025-01-08 17:09:26 +0100

    update

--- tell-me-who.mjs
+++ tell-me-who.mjs
@@ -1,16 +1,14 @@
 import { readdir } from "node:fs/promises";
+let arg = process.argv[2];
+let files = await readdir(arg);
 
-async function main() {
-  let arg = process.argv[2];
-  let files = await readdir(arg);
-  let data = files.sort((a, b) => {
-    let A = a.split("_")[0];
-    let B = b.split("_")[0];
-    return !A.localeCompare(B);
-  });
-  for (let i = 1; i <= files.length; i++) {
-    console.log(i, data[i - 1].split(".")[0].split("_")[0]);
-  }
-}
+let ff = files.map((file) => {
+  return file.split(".")[0];
+});
+ff.forEach((ele, i) => {
+  let test = ele.split("_");
+  let first = test[0];
+  let last = test[1];
 
-main().catch(console.error);
+  console.log(`${i + 1}. ${[last, first].join(" ")}`);
+});
