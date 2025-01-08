5e4f8e218ae5f12323b053f146f3c28a86f00598
aberhili <aberhili0man@gmail.com>
2025-01-08 12:18:41 +0100

    update

--- tell-me-who.mjs
+++ tell-me-who.mjs
@@ -2,5 +2,5 @@ import { readdir } from "node:fs/promises";
 let arg = process.argv[2];
 let files = await readdir(arg);
 for (let i = 1; i <= files.length; i++) {
-  console.log(i+".", files[i-1].split("_").sort().join('\n'));
+  console.log(i+".", files[i-1].split("_").join('\n'));
 }
