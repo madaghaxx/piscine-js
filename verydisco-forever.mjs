5ebc26b734b14de470b9b48a97dc477c530782f1
aberhili <aberhili0man@gmail.com>
2025-01-08 11:24:40 +0100

    update

---
+++ verydisco-forever.mjs
@@ -0,0 +1,17 @@
+import fs from "fs";
+let arg = process.argv[2];
+let revstr = (str) => {
+  let arghalf1 = str.slice(0, Math.ceil(str.length / 2));
+  let arghalf2 = str.slice(Math.ceil(str.length / 2));
+  return arghalf2 + arghalf1;
+};
+let temp = "";
+arg.split(" ").forEach((i) => {
+  temp += revstr(i) + " ";
+});
+fs.writeFile("verydisco-forever.txt", temp, (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+});
