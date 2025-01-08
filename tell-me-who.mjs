4199251eb7b896c6a1b4063819e43844dbde807f
aberhili <aberhili0man@gmail.com>
2025-01-08 12:22:48 +0100

    update

--- tell-me-who.mjs
+++ tell-me-who.mjs
@@ -2,9 +2,9 @@ import { readdir } from "node:fs/promises";
 let arg = process.argv[2];
 let files = await readdir(arg);
 files.sort((a, b) => {
-  const secondElemA = a.split("_")[0];
-  const secondElemB = b.split("_")[1];
-  return secondElemA.localeCompare(secondElemB);
+  const A = a.split("_")[0];
+  const B = b.split("_")[1];
+  return A.localeCompare(B);
 });
 for (let i = 0; i < files.length; i++) {
   const sortedName = files[i].split("_").join(" ");
