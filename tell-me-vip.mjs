9252d2e222ed3e3da7e77549f269e1d171b874cd
Aymane Berhili <aberhili0man@gmain.com>
2025-01-08 18:08:46 +0100

    update

--- tell-me-vip.mjs
+++ tell-me-vip.mjs
@@ -1,6 +1,21 @@
-import { readFile } from "fs";
-readFile(process.argv[2], "utf-8", (_, data) => {
-    let file = data.split("\n");
-    console.log(file);
-    
+import { readdir, readFile } from "fs/promises";
+import { join } from "path";
+
+let arg = process.argv[2];
+let files = await readdir(arg);
+
+let res = [];
+for (const ele of files) {
+  if (ele.endsWith(".json")) {
+    let filePath = join(arg, ele);
+    let data = await readFile(filePath, "utf8");
+    let ff = JSON.parse(data);
+      res.push(ff);
+  }
+}
+
+let gg =res.filter((a) => {
+   console.log(Object.values(a)[0])
 });
+console.log(gg);
+
