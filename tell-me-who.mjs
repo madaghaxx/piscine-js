5c79425b9611788894fb10de98a849a5a061d5f3
aberhili <aberhili0man@gmail.com>
2025-01-08 12:33:59 +0100

    update

--- tell-me-who.mjs
+++ tell-me-who.mjs
@@ -6,9 +6,7 @@ for (let i = 0; i < files.length; i++) {
     .split(".")[0]
     .split("_")
     .sort((a, b) => {
-      const A = a.split("_")[0];
-      const B = b.split("_")[1];
-      return A.localeCompare(B);
+      return a.localeCompare(b);
     })
     .join(" ");
   console.log(`${i + 1}. ${sortedName}`);
