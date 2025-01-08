1b2740903b46e508039562c5f54d2c4c2cb14d95
aberhili <aberhili0man@gmail.com>
2025-01-08 13:14:25 +0100

    update

--- tell-me-who.mjs
+++ tell-me-who.mjs
@@ -1,11 +1,16 @@
 import { readdir } from "node:fs/promises";
-let arg = process.argv[2];
-let files = await readdir(arg);
-let data = files.sort((a, b) => {
-  let A = a.split("_")[0];
-  let B = b.split("_")[0];
-  return A.localeCompare(B);
-});
-for (let i = 1; i <= files.length; i++) {
-  console.log(i, data[i - 1].split(".")[0].split("_")[0] + [1]);
+
+async function main() {
+  let arg = process.argv[2];
+  let files = await readdir(arg);
+  let data = files.sort((a, b) => {
+    let A = a.split("_")[0];
+    let B = b.split("_")[0];
+    return A.localeCompare(B);
+  });
+  for (let i = 1; i <= files.length; i++) {
+    console.log(i, data[i - 1].split(".")[0].split("_")[0]);
+  }
 }
+
+main().catch(console.error);
