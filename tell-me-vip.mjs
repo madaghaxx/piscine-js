457d7e3f552a90cd121d9186b7e0fffeb87e31b3
Aymane Berhili <aberhili0man@gmain.com>
2025-01-09 11:32:40 +0100

    update

--- tell-me-vip.mjs
+++ tell-me-vip.mjs
@@ -1,28 +1,25 @@
 import fs from "fs/promises";
 import { join } from "path";
+let arg = process.argv[2];
+let files = await fs.readdir(arg);
 
-(async () => {
-  let arg = process.argv[2];
-  let files = await fs.readdir(arg);
-
-  let text = "";
-  for (let i = 0; i < files.length; i++) {
-    if (files[i].endsWith(".json")) {
-      let filePath = join(arg, files[i]);
-      let data = await fs.readFile(filePath, "utf8");
-      let ff = JSON.parse(data);
-      if (Object.values(ff)[0] == "yes") {
-        let names = files[i].split(".")[0];
-        let name = names.split("_")[0];
-        let lastname = names.split("_")[1];
-        text += `${i + 1}. ${[lastname, name].join(" ")}`;
-      }
+let text = "";
+for (let i = 0; i < files.length; i++) {
+  if (files[i].endsWith(".json")) {
+    let filePath = join(arg, files[i]);
+    let data = await fs.readFile(filePath, "utf8");
+    let ff = JSON.parse(data);
+    if (Object.values(ff)[0] == "yes") {
+      let names = files[i].split(".")[0];
+      let name = names.split("_")[0];
+      let lastname = names.split("_")[1];
+      text += `${i + 1}. ${[lastname, name].join(" ")}`+'\n'
     }
   }
-  await fs.writeFile("vip.txt", text, (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-  });
-})();
+}
+fs.writeFile("vip.txt", text, (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+});
