8939fc0fe81cf2adc6dfe040b48a255a39780bdd
Aymane Berhili <aberhili0man@gmain.com>
2025-01-09 11:08:45 +0100

    update

--- tell-me-vip.mjs
+++ tell-me-vip.mjs
@@ -4,16 +4,11 @@ import { join } from "path";
 let arg = process.argv[2];
 let files = await readdir(arg);
 
-// let res = [];
 for (let i = 0; i < files.length; i++) {
   if (files[i].endsWith(".json")) {
     let filePath = join(arg, files[i]);
     let data = await readFile(filePath, "utf8");
     let ff = JSON.parse(data);
-    //   res.push(ff);
-    //   let gg = res.filter((a) => Object.values(a)[0] == "yes");
-    //   console.log(gg);
-    // }0
     if (Object.values(ff)[0] == "yes") {
       let names = files[i].split(".")[0];
       let name = names.split("_")[0];
