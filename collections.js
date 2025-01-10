24ac1f140f90ad6b590ca2714aa72a5f468f2742
aberhili <aberhili0man@gmail.com>
2025-01-10 16:21:04 +0100

    update

--- collections.js
+++ collections.js
@@ -8,14 +8,16 @@ let arrToSet = (arr = []) => {
   return res;
 };
 
-let arrToStr = (arr) => {
-  let res = "";
-  arr.forEach((ele) => {
-    res += ele;
-  });
-  return res;
-};
+let arrToStr = (arr) => arr.join("");
 
 let setToArr = (set) => Array.from(set);
 let setToStr = (set) => arrToStr(setToArr(set));
-let strToArr = (str)=>{}
+let strToArr = (str) => str.split(" ");
+let strToSet = (str) => strToArr(arrToSet(str));
+let mapToObj = (map) => {
+  let obj = {};
+  let i = 0;
+  while (i < map.length) {
+    
+  }
+};
