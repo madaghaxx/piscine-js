0653b61b8a0847f511236e89f067ca4adac4bf53
aberhili <aberhili0man@gmail.com>
2025-01-15 10:42:12 +0100

    update

--- collections.js
+++ collections.js
@@ -9,7 +9,6 @@ let arrToSet = (arr = []) => {
 };
 
 let arrToStr = (arr) => arr.join("");
-
 let setToArr = (set) => Array.from(set);
 let setToStr = (set) => arrToStr(setToArr(set));
 let strToArr = (str) => str.split(" ");
@@ -18,6 +17,31 @@ let mapToObj = (map) => {
   let obj = {};
   let i = 0;
   while (i < map.length) {
-    
+    obj[map[i][0]] = map[i][1];
+    i++;
   }
+};
+let objToArr = (obj) => {
+  return Object.values(obj);
+};
+
+let objToMap = (obj) => {
+  let map = new Map();
+  for (const [key, value] of Object.entries(obj)) {
+    map.set(key, value);
+  }
+  return map;
+};
+
+let arrToObj = (arr) => {
+  let obj = {};
+  for (let i = 0; i < arr.length; i++) {
+    obj[i] = arr[i];
+  }
+  return obj;
+};
+
+let strToObj = (str) => {
+  let arr = str.split("");
+  return arrToObj(arr);
 };
