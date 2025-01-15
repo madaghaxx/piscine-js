b37885e9ec35114ca39941eafa610d92bce4e673
aberhili <aberhili0man@gmail.com>
2025-01-15 17:18:36 +0100

    update

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
-#bin/bash
-git remote add github https://github.com/madaghaxx/clonernews.git
-git remote add zone01 https://learn.zone01oujda.ma/git/aberhili/clonernews.git
-git config --global credential.helper store
+# #bin/bash
+# git remote add github https://github.com/madaghaxx/clonernews.git
+# git remote add zone01 https://learn.zone01oujda.ma/git/aberhili/clonernews.git
+# git config --global credential.helper store
 git add .
 git commit -m "update"
 git push github master
